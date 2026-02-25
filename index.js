34d52e506accdb2c10fa343176b0b3c440029976
Joel Martin <joel@martindigital.design>
2026-02-25 13:29:23 +0000

    Init

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyBFvNA1RNnICrRbI9iuhHKxyhvJ_ocuwUQ',
+    authDomain: 'rc-tree-care-a6d8e.firebaseapp.com',
+    projectId: 'rc-tree-care-a6d8e',
+    storageBucket: 'rc-tree-care-a6d8e.firebasestorage.app',
+    messagingSenderId: '1094337910470',
+    appId: '1:1094337910470:web:7be929e35bcd8740b4a9c7',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
